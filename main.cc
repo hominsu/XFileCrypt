2845679d33cce8ae0a27ff05986eece9034f351d
HominSu <1774069959@qq.com>
2021-10-02 18:42:42 +0800

    👷 完成 CryptTask 任务

--- main.cc
+++ main.cc
@@ -2,15 +2,17 @@
 // Created by Homin Su on 2021/10/2.
 //
 
-#include <iostream>
 #include <sstream>
 #include <memory>
 #include <memory_resource>
 
 #include "x_thread_pool/x_thread_pool.h"
 #include "task/read/read_task.h"
+#include "task/crypt/crypt_task.h"
 
 int main() {
+  std::string password = "12345678";
+
   XThreadPool::Get()->Init();
 
   // 创建线程安全的内存池
@@ -20,12 +22,29 @@ int main() {
   read_task_1->Init("../test/src/WechatIMG1.jpeg");
   read_task_1->set_memory_resource(memory_resource);
 
+  auto crypt_task_1 = std::make_shared<CryptTask>();
+  crypt_task_1->set_memory_resource(memory_resource);
+  crypt_task_1->Init(password);
+  crypt_task_1->set_is_encrypt(true);
+
+  read_task_1->set_next(crypt_task_1);
+
+  XThreadPool::Get()->AddTask(read_task_1);
+  XThreadPool::Get()->AddTask(crypt_task_1);
+
   auto read_task_2 = std::make_shared<ReadTask>();
   read_task_2->Init("../test/src/test.txt");
   read_task_2->set_memory_resource(memory_resource);
 
-  XThreadPool::Get()->AddTask(read_task_1);
+  auto crypt_task_2 = std::make_shared<CryptTask>();
+  crypt_task_2->set_memory_resource(memory_resource);
+  crypt_task_2->Init(password);
+  crypt_task_2->set_is_encrypt(true);
+
+  read_task_2->set_next(crypt_task_2);
+
   XThreadPool::Get()->AddTask(read_task_2);
+  XThreadPool::Get()->AddTask(crypt_task_2);
 
   read_task_1->get_return();
   read_task_2->get_return();
---
+++ task/crypt/crypt_task.h
@@ -0,0 +1,40 @@
+//
+// Created by Homin Su on 2021/10/2.
+//
+
+#ifndef XFILECRYPT_TASK_CRYPT_CRYPT_TASK_H_
+#define XFILECRYPT_TASK_CRYPT_CRYPT_TASK_H_
+
+#include "../task_base.h"
+
+class DesCrypt;
+
+/**
+ * @brief 加解密任务
+ */
+class CryptTask : public TaskBase {
+ private:
+  std::shared_ptr<DesCrypt> crypt_; ///< 加解密对象指针
+  bool is_encrypt_ = true;  ///< 加密解密标识
+
+ public:
+  /**
+   * @brief 初始化加密任务
+   * @param _password 密码
+   */
+  void Init(const std::string &_password);
+
+  /**
+   * @brief 设置加密或解密
+   * @param _is_encrypt 加密或解密
+   */
+  void set_is_encrypt(bool _is_encrypt);
+
+ private:
+  /**
+   * @brief 线程主函数
+   */
+  void Main() override;
+};
+
+#endif //XFILECRYPT_TASK_CRYPT_CRYPT_TASK_H_
