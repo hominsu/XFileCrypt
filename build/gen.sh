90a8d41ee6cda6ba7b8b49951e5ac40fea8c188e
HominSu <1774069959@qq.com>
2021-12-20 11:07:27 +0800

    👷 Update generate shell script

--- build/gen.sh
+++ build/gen.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/opt/homebrew/Cellar/gcc/11.2.0_2/bin/gcc-11 -DCMAKE_CXX_COMPILER=/opt/homebrew/Cellar/gcc/11.2.0_2/bin/g++-11 ..
+CMAKE_BUILD_TYPE=Release
+UNAME_ALL=$(uname -a)
+SYSTEM=${UNAME_ALL%% *}
 
-printf "\nUsing %d cores to build ...\n\n" "$(nproc)"
+echo -e "\e[1;36m[ info ]: Current system: $SYSTEM\e[0m\n"
 
-cmake --build . -j"$(nproc)"
-printf "\nBuild finished ...\n\n"
+echo -e "\e[1;36m[ info ]: Using $(nproc) cores to build ...\e[0m"
+
+if [[ $SYSTEM == "Darwin" ]]; then
+  cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_C_COMPILER=/opt/homebrew/Cellar/gcc/11.2.0_3/bin/gcc-11 -DCMAKE_CXX_COMPILER=/opt/homebrew/Cellar/gcc/11.2.0_3/bin/g++-11 ..
+  cmake --build . --config $CMAKE_BUILD_TYPE -j"$(nproc)"
+fi
+
+echo -e "\e[1;36m[ info ]: Build finished ...\e[0m\n"
+
+echo -e "\e[1;36m[ info ]: Start CTest ...\e[0m"
+ctest -C $CMAKE_BUILD_TYPE
+
+echo -e "\n\e[1;36m[ info ]: CTest finished ...\e[0m"
